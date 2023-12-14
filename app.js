0d4043754d507ea80cb850a2ec3fce26f0c414f0
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-12-14 18:51:17 -0300

    Imprimindo numero secreto no console

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
+let numeroSecreto = gerarNumeroAleatorio();
+
 const botaoChutar = document.querySelector(".botao-chutar");
 
 const numeroMenor = 1;
 const numeroMaior = 10;
 
 function exibirTextoNaTela(classHtml, texto) {
-    let campo = document.querySelector(classHtml);
-    campo.innerHTML = texto;
+  let campo = document.querySelector(classHtml);
+  campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('.titulo-jogo', 'Jogo do número secreto');
-exibirTextoNaTela('.texto__paragrafo', `Escolha um número entre ${numeroMenor} e ${numeroMaior}`)
+exibirTextoNaTela(".titulo-jogo", "Jogo do número secreto");
+exibirTextoNaTela(
+  ".texto__paragrafo",
+  `Escolha um número entre ${numeroMenor} e ${numeroMaior}`
+);
+
+function verificarChute() {
+  console.log(numeroSecreto);
+}
 
 botaoChutar.addEventListener("click", () => {
-  console.log("O botão foi clicado!");
+  verificarChute();
 });
+
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * 10 + 1);
+}
