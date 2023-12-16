f6a2bd2ab615ea9f4295810925896738d35d89d7
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-12-16 14:11:49 -0300

    testes

---
+++ teste.js
@@ -0,0 +1,14 @@
+const frase = "olá João";
+const palavraBuscada = "olá";
+
+function buscaPalavra() {
+  if (frase.includes(palavraBuscada)) {
+    console.log(`A palavra "${palavraBuscada}" está presente na frase "${frase}".`);
+  } else {
+    console.log(
+      `A palavra "${palavraBuscada}" não está presente na frase "${frase}".`
+    );
+  }
+}
+
+console.log(buscaPalavra());
