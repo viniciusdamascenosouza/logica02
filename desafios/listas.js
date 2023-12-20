a3cd14e7005f4489612fab705cc626be65a0ba68
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-12-20 11:16:54 -0300

    desafio listas

---
+++ desafios/listas.js
@@ -0,0 +1,15 @@
+// PARTE 1
+const listaGenerica = [];
+
+// PARTE 2
+const linguagensDeProgramacao = ["Java", "Ruby", "Go", "JavaScript", "PHP"];
+
+// PARTE 3
+linguagensDeProgramacao.push("Python", "Kotlin", "C#", "C", "C++");
+
+// PARTE 4
+const tartarugasNijas = ["Leonardo", "Donatello", "Michelangelo", "Rafael"];
+console.log(tartarugasNijas[0]);
+console.log(tartarugasNijas[1]);
+console.log(tartarugasNijas[2]);
+console.log(tartarugasNijas[3]);
